d8619af7ae27445ca770c5cca1a02acc17a1dd34
Puhovon <91030077+Puhovon@users.noreply.github.com>
2023-02-21 22:47:32 +0300

    конечные изменения

---
+++ WpfApp1/Data/Persones.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Persones] (
+    [id]          INT           IDENTITY (1, 1) NOT NULL,
+    [secondName]  VARCHAR (55)  NOT NULL,
+    [firstName]   VARCHAR (55)  NOT NULL,
+    [lastName]    VARCHAR (55)  NULL,
+    [phoneNumber] VARCHAR (55)  NULL,
+    [email]       VARCHAR (100) NOT NULL
+);
+
